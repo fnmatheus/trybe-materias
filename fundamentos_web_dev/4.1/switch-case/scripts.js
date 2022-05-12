4a6d737bc54b762abce9413f43a916fdaec2d2b7
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-05-12 18:30:10 -0300

    aula 4.1

---
+++ fundamentos_web_dev/4.1/switch-case/scripts.js
@@ -0,0 +1,18 @@
+let estadoNoProcessoSeletivo = '';
+
+switch (estadoNoProcessoSeletivo){
+    case 'aprovada':
+        console.log('aprovada')
+        break;
+    
+    case 'lista':
+        console.log('lista')
+        break;
+    
+    case 'reprovada':
+        console.log('reprovada')
+        break;
+
+    default:
+        console.log('não se aplica')
+}
